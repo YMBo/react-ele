bfcfd5ba80517b9fc6a58197b41ade1364fa61e6
YMBo <619697451@qq.com>
2017-09-14 11:43:01 +0800

    添加星级评级

--- src/components/returnTop/returnTop.js
+++ src/components/returnTop/returnTop.js
@@ -47,7 +47,9 @@ class ReturnTop extends Component{
 	}
 	render(){
 		return(
-			<div className='returnTop' onClick={this.handleGoBack.bind(this)} style={{opacity:`${this.state.opa?0:1}`,display:`${this.state.dis?'none':'block'}`}}></div>
+			<div className='returnTop' onClick={this.handleGoBack.bind(this)} style={{opacity:`${this.state.opa?0:1}`,display:`${this.state.dis?'none':''}`}}>
+				<svg className="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><defs></defs><path d="M32 0h960v156.8H32V0z m960 710.4h-240V1024h-480v-313.6H32L512 236.8l480 473.6z"  fill="#999999"></path></svg>
+			</div>
 		)
 	}
 }
